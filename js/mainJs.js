1b40f115d7686999934b8263524a04b10088d9a0
victorpinon <victorpinonrattia@hotmail.com>
2018-09-28 09:10:25 +0200

    Fixed bugs and changed from .getJson to .ajax

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-  
   $("#search").click(function(){
     var username = $('#user').val();
-    $.getJSON("https://api.github.com/users/"+username+"/repos", function(result){
-
-      var key, count = 0;
-      for(key in result) {
-        $(".repos").append("<tr>\
-                          <td>"+result[count].name+"</td>\
-                          <td>"+result[count].description+"</td>\
-                          <td>"+result[count].watchers+"</td>\
-                        </tr>");
-        count++
+    $.ajax({ 
+      type: 'GET', 
+      url: "https://api.github.com/users/"+username+"/repos", 
+      data: { get_param: 'value' }, 
+      dataType: 'json',
+      success: function (result) {
+        $(".repos").html("");
+        var key, count = 0;
+        for(key in result) {
+          $(".repos").append("<tr>\
+                            <td>"+result[count].name+"</td>\
+                            <td>"+result[count].description+"</td>\
+                            <td>"+result[count].watchers+"</td>\
+                          </tr>");
+          count++
+        }
+          
       }
     });
-});
+  });
+
 
 });
